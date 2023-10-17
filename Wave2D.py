07d638660274a0d66844be3f8af1e7a92a13ad22
mustapho <143328060+mustapho@users.noreply.github.com>
2023-10-18 00:10:05 +0200

    Update Wave2D.py

--- Wave2D.py
+++ Wave2D.py
@@ -8,19 +8,30 @@ x, y, t = sp.symbols('x,y,t')
 
 class Wave2D:
 
-    def create_mesh(self, N, sparse=False):
+    def create_mesh(self, N, L=1, sparse=False):
         """Create 2D mesh and store in self.xij and self.yij"""
-        # self.xji, self.yij = ...
-        raise NotImplementedError
+        self.L = L
+        self.h = L/N
+        x = y = np.linspace(0,L,N+1)
+        self.xij, self.yij = np.meshgrid(x,y,indexing = 'ij')
+        return self.xij, self.yij
+        #raise NotImplementedError
 
     def D2(self, N):
         """Return second order differentiation matrix"""
-        raise NotImplementedError
+        D = sparse.diags([1, -2, 1], [-1, 0, 1], (N+1, N+1), 'lil')
+        D[0, :4] = 2, -5, 4, -1
+        D[-1, -4:] = -1, 4, -5, 2
+        return (1/self.h**2)*D
+        #raise NotImplementedError
 
     @property
     def w(self):
         """Return the dispersion coefficient"""
-        raise NotImplementedError
+        kx = self.mx*sp.pi
+        ky = self.my*sp.pi
+        return self.c*sp.sqrt(kx**2+ky**2)
+        #raise NotImplementedError
 
     def ue(self, mx, my):
         """Return the exact standing wave"""
@@ -36,12 +47,23 @@ class Wave2D:
         mx, my : int
             Parameters for the standing wave
         """
-        raise NotImplementedError
+        Unp1, Un, Unm1 = np.zeros((3, N+1, N+1))
+        xij, yij = self.create_mesh(N)
+        dt = self.dt
+        
+        ue_numeric = sp.lambdify((x,y,t),self.ue(self.mx,self.my),'numpy')(xij,yij,0)
+        Unm1[:] = ue_numeric
+        Un[:] = Unm1[:] + 0.5*(self.c*dt)**2*(self.D2(N) @ Unm1 + Unm1 @ self.D2(N).T)
+        
+        self.Unm1 = Unm1
+        self.Un = Un
+        self.Unp1 = Unp1
+        #raise NotImplementedError
 
-    @property
+     @property
     def dt(self):
         """Return the time step"""
-        raise NotImplementedError
+        return self.cfl*self.h/self.c
 
     def l2_error(self, u, t0):
         """Return l2-error norm
@@ -53,10 +75,17 @@ class Wave2D:
         t0 : number
             The time of the comparison
         """
-        raise NotImplementedError
+        xij, yij = self.create_mesh(self.N)
+        ue_t0 = sp.lambdify((x,y,t),self.ue(self.mx,self.my),'numpy')(xij,yij,t0)
+        return np.sqrt(self.h**2*np.sum((ue_t0-u)**2))
+        #raise NotImplementedError
 
     def apply_bcs(self):
-        raise NotImplementedError
+        self.Unp1[0] = 0
+        self.Unp1[-1] = 0
+        self.Unp1[:, -1] = 0
+        self.Unp1[:, 0] = 0
+        #raise NotImplementedError
 
     def __call__(self, N, Nt, cfl=0.5, c=1.0, mx=3, my=3, store_data=-1):
         """Solve the wave equation
@@ -83,7 +112,39 @@ class Wave2D:
         If store_data > 0, then return a dictionary with key, value = timestep, solution
         If store_data == -1, then return the two-tuple (h, l2-error)
         """
-        raise NotImplementedError
+        self.N = N
+        self.Nt = Nt
+        self.cfl = cfl
+        self.c = c
+        self.mx = mx
+        self.my = my
+        xij, yij = self.create_mesh(N)
+        dt = self.dt
+        D = self.D2(N)
+        self.initialize(N, mx, my)
+        Unm1 = self.Unm1
+        Un = self.Un
+        Unp1 = self.Unp1
+
+    
+        plotdata = {0: self.Unm1.copy()}
+        if store_data == 1:
+            plotdata[1] = self.Un.copy()
+        for n in range(1, Nt):
+            Unp1[:] = 2*Un[:] - Unm1[:] + (c*dt)**2*(D @ Un + Un @ D.T)
+            # Set boundary conditions
+            self.apply_bcs()
+            # Swap solutions
+            Unm1[:] = Un
+            Un[:] = Unp1
+            if n % store_data == 0:
+                plotdata[n] = Unm1.copy() # Unm1 is now swapped to Un
+        if store_data > 0:
+            return xij, yij, plotdata
+        if store_data == -1:
+            #print(self.h, [self.l2_error(Unm1,(Nt-1)*dt)])
+            return (self.h, [self.l2_error(Unm1,(Nt-1)*dt)])
+        #raise NotImplementedError
 
     def convergence_rates(self, m=4, cfl=0.1, Nt=10, mx=3, my=3):
         """Compute convergence rates for a range of discretizations
@@ -121,13 +182,19 @@ class Wave2D:
 class Wave2D_Neumann(Wave2D):
 
     def D2(self, N):
-        raise NotImplementedError
+        D = sparse.diags([1, -2, 1], [-1, 0, 1], (N+1, N+1), 'lil')
+        D[0, :4] = -2, 2, 0, 0
+        D[-1, -4:] = 0, 0, 2, -2
+        return (1/self.h**2)*D
+        #raise NotImplementedError
 
     def ue(self, mx, my):
-        raise NotImplementedError
+        return sp.cos(mx*sp.pi*x)*sp.cos(my*sp.pi*y)*sp.cos(self.w*t)
+        #raise NotImplementedError
 
     def apply_bcs(self):
-        raise NotImplementedError
+        return
+        #raise NotImplementedError
 
 def test_convergence_wave2d():
     sol = Wave2D()
@@ -140,4 +207,35 @@ def test_convergence_wave2d_neumann():
     assert abs(r[-1]-2) < 0.05
 
 def test_exact_wave2d():
-    raise NotImplementedError
+    Dsol = Wave2D()
+    Derror = Dsol.convergence_rates(m=8,cfl=1/np.sqrt(2),mx = 3,my = 3)[1]
+    #Had to use more discretization levels (m=8) on this one.
+    Nsol = Wave2D_Neumann()
+    Nerror = Nsol.convergence_rates(cfl=1/np.sqrt(2),mx = 3,my = 3)[1]
+    assert abs(Derror[-1] < 1e-12)
+    assert abs(Nerror[-1] < 1e-12)
+
+
+def wave2d_animation():
+    import matplotlib.animation as animation
+    sol = Wave2D_Neumann()
+    xij, yij, data = sol(N = 40, Nt = 500, cfl = 0.5, c= 1, store_data=5)
+
+    fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+    frames = []
+    for n, val in data.items():
+        frame = ax.plot_wireframe(xij, yij, val, rstride=2, cstride=2);
+        #frame = ax.plot_surface(xij, yij, val, vmin=-0.5*data[0].max(),
+        #                        vmax=data[0].max(), cmap=cm.coolwarm,
+        #                        linewidth=0, antialiased=False)
+        frames.append([frame])
+
+    ani = animation.ArtistAnimation(fig, frames, interval=400, blit=True,
+                                repeat_delay=1000)
+    ani.save('neumannwave.gif', writer='pillow', fps=5) # This animated png opens in a browser
+
+
+if __name__ == '__main__':
+    test_convergence_wave2d()
+    test_convergence_wave2d_neumann()
+    test_exact_wave2d()
